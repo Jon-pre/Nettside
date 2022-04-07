433a439588f2707e7bad371ab9155e689ff4fbde
Erebrias <70891750+Erebrias@users.noreply.github.com>
2022-04-07 21:13:56 +0200

    Første commit, lå oss se hvordan dette går

---
+++ src/main/java/com/example/nettside/NettsideApplication.java
@@ -0,0 +1,13 @@
+package com.example.nettside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettsideApplication.class, args);
+    }
+
+}
